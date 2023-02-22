b8cc932601da1361dca86388b24676304d52a08e
Barış Benli <bb.sosyal@pm.me>
2023-02-23 00:17:06 +0300

    redux iş takip projesi

--- redux-is-takip/src/slices/employeesSlice.js
+++ redux-is-takip/src/slices/employeesSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-
+import axios from "axios";
 const initialState = {
   employees: [],
   employeeDetail: [],
@@ -8,32 +8,35 @@ const initialState = {
 export const fetchEmployees = createAsyncThunk(
   "employee/fetchEmployees",
   async () => {
-    const response = await fetch("http://localhost:5000/employees").then(
-      (response) => response.json()
-    );
-    return response;
+    const response = await axios.get("http://localhost:5000/employees");
+    return response.data;
   }
 );
 export const postEmloyees = createAsyncThunk(
   "employee/postEmloyee",
   async (data) => {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-    await fetch("http://localhost:5000/employees", options);
+    const response = await axios.post("http://localhost:5000/employees", data);
+    return response.data;
   }
 );
 export const deleteEmployees = createAsyncThunk(
   "employee/deleteEmployees",
   async (id) => {
-    const options = {
-      method: "DELETE",
-    };
-    await fetch(`http://localhost:5000/employees/${id}`, options);
+    const response = await axios.delete(
+      `http://localhost:5000/employees/${id}`
+    );
+    return response.data;
+  }
+);
+
+export const updateEmployee = createAsyncThunk(
+  "employee/updateEmployee",
+  async (data) => {
+    const response = await axios.put(
+      `http://localhost:5000/employees/${data.id}`,
+      data
+    );
+    return response.data;
   }
 );
 
@@ -49,19 +52,55 @@ export const employeesSlice = createSlice({
         (employee) => employee.id !== action.payload
       );
     },
+    setSelectedData: (state, action) => {
+      state.employeeDetail = action.payload;
+    },
+    resetSelectedData: (state, action) => {
+      state.employeeDetail = [];
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchEmployees.fulfilled, (state, action) => {
       state.employees = action.payload;
     });
     builder.addCase(postEmloyees.fulfilled, (state, action) => {
-      state.employees = action.payload;
+      state.employees.push(action.payload);
     });
     builder.addCase(deleteEmployees.fulfilled, (state, action) => {
-      state.employees = action.payload;
+      state.employees = state.employees.filter(
+        (employee) => employee.id !== action.payload.id
+      );
+    });
+    builder.addCase(updateEmployee.fulfilled, (state, action) => {
+      // state.employees = state.employees.filter(
+      //   (employee) => employee.id === action.payload.id
+      // );
+      state.employees.map((employee) => {
+        if (employee.id === action.payload.id) {
+          employee.employeeId = action.payload.employeeId;
+          employee.employeeFirstName = action.payload.employeeFirstName;
+          employee.employeeLastName = action.payload.employeeLastName;
+          employee.employeeGender = action.payload.employeeGender;
+          employee.employeeAge = action.payload.employeeAge;
+          employee.employeePhone = action.payload.employeePhone;
+          employee.employeeEmail = action.payload.employeeEmail;
+          employee.employeePassword = action.payload.employeePassword;
+          employee.employeeAddress = action.payload.employeeAddress;
+          employee.employeeStatus = action.payload.employeeStatus;
+          employee.employeeRating = action.payload.employeeRating;
+          return employee;
+        } else {
+          return employee;
+        }
+      });
     });
   },
 });
 
-export const { addEmployee, deleteEmployee } = employeesSlice.actions;
+export const {
+  addEmployee,
+  deleteEmployee,
+  setSelectedData,
+  resetSelectedData,
+} = employeesSlice.actions;
 export default employeesSlice.reducer;
